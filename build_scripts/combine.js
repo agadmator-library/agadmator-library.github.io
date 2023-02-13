b586198e7c7407e6f1daeb6a1fbe3a4ba390857b
Mateusz Dziurdziak <8903656+mateusz-dziurdziak@users.noreply.github.com>
2023-02-13 16:42:55 +0100

    Added gh workflow

--- build_scripts/combine.js
+++ build_scripts/combine.js
@@ -14,84 +14,79 @@ import {
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const db = []
-const pgns = {}
-const allPgns = []
+export function combine() {
+    const db = []
+    const pgns = {}
+    const allPgns = []
 
-dbGetAllIds().forEach(id => {
-    const videoSnippet = dbRead(NAMESPACE_VIDEO_SNIPPET, id)
-    if (!videoSnippet) {
-        return
-    }
+    dbGetAllIds().forEach(id => {
+        const videoSnippet = dbRead(NAMESPACE_VIDEO_SNIPPET, id)
+        if (!videoSnippet) {
+            return
+        }
 
-    let game = dbRead(NAMESPACE_VIDEO_GAME, id)
-    if (!game) {
-        game = null
-    }
+        let game = dbRead(NAMESPACE_VIDEO_GAME, id)
+        if (!game) {
+            game = null
+        }
 
-    db.push({
-        date: videoSnippet.publishedAt,
-        title: videoSnippet.title,
-        id: videoSnippet.videoId,
-        game: game ? {w: game.playerWhite, b: game.playerBlack} : null
+        db.push({
+            date: videoSnippet.publishedAt,
+            title: videoSnippet.title,
+            id: videoSnippet.videoId,
+            game: game ? {w: game.playerWhite, b: game.playerBlack} : null
+        })
+
+        if (game && game.pgn) {
+            pgns[videoSnippet.videoId] = game.pgn
+            allPgns.push(game.pgn)
+        }
+
     })
 
-    if (game && game.pgn) {
-        pgns[videoSnippet.videoId] = game.pgn
-        allPgns.push(game.pgn)
-    }
+    fs.writeFileSync(__dirname + '/../db.json', JSON.stringify(db))
+    fs.writeFileSync(__dirname + '/../pgns.json', JSON.stringify(pgns))
 
-})
+    let openings = JSON.parse(fs.readFileSync(__dirname + '/../openings.json', {encoding: 'utf8'}))
+        .filter(opening => {
+            return _.some(allPgns, videoPgn => _.startsWith(videoPgn, opening.moves))
+        })
+        .map(opening => {
+            return {
+                name: `${opening.eco} - ${opening.name}`,
+                moves: opening.moves
+            }
+        })
+    fs.writeFileSync(__dirname + '/../openings-slim.json', JSON.stringify(openings))
 
-fs.writeFileSync(__dirname + '/../db.json', JSON.stringify(db))
-fs.writeFileSync(__dirname + '/../pgns.json', JSON.stringify(pgns))
 
-let openings = JSON.parse(fs.readFileSync(__dirname + '/../openings.json', {encoding: 'utf8'}))
-    .filter(opening => {
-        return _.some(allPgns, videoPgn => _.startsWith(videoPgn, opening.moves))
-    })
-    .map(opening => {
-        return {
-            name: `${opening.eco} - ${opening.name}`,
-            moves: opening.moves
+    const results = {}
+    dbGetAllIds().forEach(id => {
+        const chessComResult = dbRead(NAMESPACE_CHESS_COM, id)
+        const chesstempoComResult = dbRead(NAMESPACE_CHESSTEMPO_COM, id)
+
+        function translateChessComResult(text) {
+            if (text === "1-0") {
+                return "w"
+            } else if (text === "0-1") {
+                return "b"
+            } else if (text === "½-½") {
+                return "d"
+            } else {
+                return null
+            }
+        }
+
+        const result = chessComResult && chessComResult.result
+            ? translateChessComResult(chessComResult.result)
+            : (chesstempoComResult ? chesstempoComResult.result : null)
+
+        if (result) {
+            results[id] = result
         }
     })
-fs.writeFileSync(__dirname + '/../openings-slim.json', JSON.stringify(openings))
+    fs.writeFileSync(__dirname + '/../results.json', JSON.stringify(results))
+}
 
+combine();
 
-const results = {}
-dbGetAllIds().forEach(id => {
-    const chessComResult = dbRead(NAMESPACE_CHESS_COM, id)
-    const chesstempoComResult = dbRead(NAMESPACE_CHESSTEMPO_COM, id)
-
-    function translateChessComResult(text) {
-        if (text === "1-0") {
-            return "w"
-        } else if (text === "0-1") {
-            return "b"
-        } else if (text === "½-½") {
-            return "d"
-        } else {
-            return null
-        }
-    }
-
-    const result = chessComResult && chessComResult.result
-        ? translateChessComResult(chessComResult.result)
-        : (chesstempoComResult ? chesstempoComResult.result : null)
-
-    if (result) {
-        results[id] = result
-    }
-})
-fs.writeFileSync(__dirname + '/../results.json', JSON.stringify(results))
-
-// fs.readdirSync(__dirname + '/../db/chess-com-games').forEach(fileName => {
-//     const chessCom = JSON.parse(fs.readFileSync(__dirname + '/../db/chess-com-games/' + fileName, {encoding: 'utf8'}));
-//     dbSave(NAMESPACE_CHESS_COM, fileName.replaceAll(".json", ""), chessCom)
-// })
-
-// fs.readdirSync(__dirname + '/../db/chess-tempo-games').forEach(fileName => {
-//     const chessTempo = JSON.parse(fs.readFileSync(__dirname + '/../db/chess-tempo-games/' + fileName, {encoding: 'utf8'}));
-//     dbSave(NAMESPACE_CHESSTEMPO_COM, fileName.replaceAll(".json", ""), chessTempo)
-// })
--- build_scripts/loadNewMovies.js
+++ build_scripts/loadNewMovies.js
@@ -1,72 +1,74 @@
 import {google} from "googleapis";
 import {dbRead, dbSave, NAMESPACE_VIDEO_SNIPPET} from "./db.js";
 
-console.log("Starting downloading new movies")
+export async function loadNewMovies() {
+    console.log("Starting downloading new movies")
 
-let youtube = google.youtube({
-    version: 'v3',
-    auth: process.env.YOUTUBE_API_KEY
-});
+    let youtube = google.youtube({
+        version: 'v3',
+        auth: process.env.YOUTUBE_API_KEY
+    });
 
-console.log("Downloading channel contentDetails")
+    console.log("Downloading channel contentDetails")
 
-const channelContentDetails = await youtube.channels.list({
-    forUsername: 'agadmator',
-    part: ['contentDetails']
-});
+    const channelContentDetails = await youtube.channels.list({
+        forUsername: 'agadmator',
+        part: ['contentDetails']
+    });
 
-console.log("Downloaded channel contentDetails")
+    console.log("Downloaded channel contentDetails")
 
-const uploadPlaylistId = channelContentDetails
-    .data
-    .items
-    .find(value => value.contentDetails.relatedPlaylists.uploads != null)
-    .contentDetails
-    .relatedPlaylists
-    .uploads
+    const uploadPlaylistId = channelContentDetails
+        .data
+        .items
+        .find(value => value.contentDetails.relatedPlaylists.uploads != null)
+        .contentDetails
+        .relatedPlaylists
+        .uploads
 
-console.log("Downloading playlist items")
+    console.log("Downloading playlist items")
 
-let playlistItemsResponse = await youtube.playlistItems.list({
-    playlistId: uploadPlaylistId,
-    maxResults: 10,
-    part: ['snippet']
-});
-let reachedExisting = false
-const downloadedVideosIds = []
-do {
-    const now = new Date().toISOString()
-    playlistItemsResponse.data.items
-        .filter(playlistItem => playlistItem.snippet.resourceId.videoId != null)
-        .forEach(playlistItem => {
-            const toSave = {
-                schemaVersion: 1,
-                retrievedAt: now,
-                publishedAt: playlistItem.snippet.publishedAt,
-                title: playlistItem.snippet.title,
-                description: playlistItem.snippet.description,
-                thumbnail: playlistItem.snippet.thumbnails,
-                videoId: playlistItem.snippet.resourceId.videoId
-            }
+    let playlistItemsResponse = await youtube.playlistItems.list({
+        playlistId: uploadPlaylistId,
+        maxResults: 10,
+        part: ['snippet']
+    });
+    let reachedExisting = false
+    const downloadedVideosIds = []
+    do {
+        const now = new Date().toISOString()
+        playlistItemsResponse.data.items
+            .filter(playlistItem => playlistItem.snippet.resourceId.videoId != null)
+            .forEach(playlistItem => {
+                const toSave = {
+                    schemaVersion: 1,
+                    retrievedAt: now,
+                    publishedAt: playlistItem.snippet.publishedAt,
+                    title: playlistItem.snippet.title,
+                    description: playlistItem.snippet.description,
+                    thumbnail: playlistItem.snippet.thumbnails,
+                    videoId: playlistItem.snippet.resourceId.videoId
+                }
 
-            if (dbRead(NAMESPACE_VIDEO_SNIPPET, toSave.videoId)) {
-                reachedExisting = true
-            } else {
-                dbSave(NAMESPACE_VIDEO_SNIPPET, toSave.videoId, toSave)
-                downloadedVideosIds.push(toSave.videoId)
-            }
-        })
-    if (!reachedExisting) {
-        playlistItemsResponse = await youtube.playlistItems.list({
-            playlistId: uploadPlaylistId,
-            maxResults: 50,
-            part: ['snippet'],
-            pageToken: playlistItemsResponse.data.nextPageToken,
-        })
-    }
-} while (!reachedExisting && playlistItemsResponse.data.nextPageToken != null)
+                if (dbRead(NAMESPACE_VIDEO_SNIPPET, toSave.videoId)) {
+                    reachedExisting = true
+                } else {
+                    dbSave(NAMESPACE_VIDEO_SNIPPET, toSave.videoId, toSave)
+                    downloadedVideosIds.push(toSave.videoId)
+                }
+            })
+        if (!reachedExisting) {
+            playlistItemsResponse = await youtube.playlistItems.list({
+                playlistId: uploadPlaylistId,
+                maxResults: 50,
+                part: ['snippet'],
+                pageToken: playlistItemsResponse.data.nextPageToken,
+            })
+        }
+    } while (!reachedExisting && playlistItemsResponse.data.nextPageToken != null)
 
-console.log('Downloaded ' + downloadedVideosIds.length + ' videos')
-console.log('Downloaded videos ids are: ' + JSON.stringify(downloadedVideosIds))
+    console.log('Downloaded ' + downloadedVideosIds.length + ' videos')
+    console.log('Downloaded videos ids are: ' + JSON.stringify(downloadedVideosIds))
 
-
+    return downloadedVideosIds
+}
