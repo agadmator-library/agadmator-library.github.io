4e1792f57ca36e2bc059eb1bcbafe660fcd444cc
Mateusz Dziurdziak <8903656+mateusz-dziurdziak@users.noreply.github.com>
2023-02-26 08:11:21 +0100

    levon Aronian -> Levon Aronian

--- generated/js/references.js
+++ generated/js/references.js
@@ -1,5 +1,5 @@
 var references={
-  "db": "db-03a9ac4a9bf40a225e467ed0ce07fb387fa01cdd.json",
+  "db": "db-912411764b738824dcd1fc055ffa492dddb019c3.json",
   "pgns": "pgns-6dd1da5f71f44a4e88d7efa9ae219ac03f446299.json",
   "videoLength": "videoLength-7ec0acf6d6147b6199c8bf0eb0291d3f282b80e2.json",
   "positions": "positions-6b4457c3f5f0e97582bedb1ae0908c13d720a549.json",
