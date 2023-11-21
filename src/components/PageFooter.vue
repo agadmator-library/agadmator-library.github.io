9cc229371672d7ad20693c94ba7d6703deb7b950
Mateusz Dziurdziak <8903656+mateusz-dziurdziak@users.noreply.github.com>
2023-11-21 14:35:03 +0100

    Page footer

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -3,7 +3,15 @@
 </script>
 
 <template>
-
+  <footer class="page-footer font-small indigo">
+    <div class="footer-copyright text-center py-3">This is only a fun project and not everything is properly parsed. Do
+      not trust entirely.
+    </div>
+    <div class="footer-copyright text-center py-3">Report missing/invalid data by creating an issue on <a
+        href="https://github.com/agadmator-library/agadmator-library.github.io/issues"
+        target="_blank">GitHub</a>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
